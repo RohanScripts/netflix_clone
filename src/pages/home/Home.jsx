5aefe54aafc772e9de52ef0cb493302bc803adc1
RohanScripts <shelarrohan1810@gmail.com>
2024-08-08 01:46:57 +0530

    chore: Update navbar styling and gap in Navbar.css, and hide scrollbar in index.css
    This commit updates the navbar styling in Navbar.css by increasing the gap between list items from 1vw to 1.5vw. It also hides the scrollbar in index.css by adding the CSS rule `body::-webkit-scrollbar { display: none; }`.
    
    These changes improve the visual appearance and user experience of the application's navbar and page scrolling.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="hero">
+        <img src={hero_banner} alt="" className="hero-banner" srcset="" />
+        <div className="hero-caption">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab illum
+            laudantium nisi ullam magnam corrupti labore incidunt fuga, commodi
+            soluta a aliquam corporis fugiat quasi perspiciatis repellat. Id,
+            officiis non.
+          </p>
+          <div className="hero-buttons">
+            <button>
+              <img src={play_icon} alt="" srcset="" />
+              Play
+            </button>
+            <button>
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
